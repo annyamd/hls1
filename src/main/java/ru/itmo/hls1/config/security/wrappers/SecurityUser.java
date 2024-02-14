a4c818115dcbae9d50622728d43032efe99112f8
Kim Daniel <kimdaniiell@gmail.com>
2024-02-14 09:24:20 +0300

    Added SecurityUser wrapper-class implementing <UserDetails> interface

---
+++ src/main/java/ru/itmo/hls1/config/security/wrappers/SecurityUser.java
@@ -0,0 +1,53 @@
+package ru.itmo.hls1.config.security.wrappers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import ru.itmo.hls1.model.entity.User;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+public class SecurityUser  implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRoles()
+                .stream()
+                .map(SecurityRole::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
